89b36355c4c22d4e0917fc074ab042eccd932e65
Rene Hexel <r.hexel@griffith.edu.au>
2017-11-19 08:12:50 +1000

    Add executable target

--- Package.swift
+++ Package.swift
@@ -6,23 +6,15 @@ import PackageDescription
 let package = Package(
     name: "SwiftMosquitto",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SwiftMosquitto",
-            targets: ["SwiftMosquitto"]),
+        .library(name: "SwiftMosquitto", targets: ["SwiftMosquitto"]),
+        .executable(name: "mosquitto-client", targets: ["mosquitto-client"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+        .package(url: "https://github.com/rhx/Clibmosquitto.git", .branch("master")),
+   ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SwiftMosquitto",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftMosquittoTests",
-            dependencies: ["SwiftMosquitto"]),
+        .target(name: "SwiftMosquitto", dependencies: ["Clibmosquitto"]),
+        .target(name: "mosquitto-client", dependencies: ["SwiftMosquitto"]),
+        .testTarget(name: "SwiftMosquittoTests", dependencies: ["SwiftMosquitto"]),
     ]
 )
